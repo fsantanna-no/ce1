3a5d6c29a9b943b441e7a3adc11356b89336a445
Francisco Sant'Anna <francisco.santanna@gmail.com>
2022-03-02 19:43:50 -0300

    - ce0

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -224,6 +224,30 @@ open class Parser
         return e
     }
 
+    fun set_tail (tk0: Tk.Chr, dst: Expr): Stmt {
+        return when {
+            all.accept(TK.INPUT) -> {
+                val tk = all.tk0 as Tk.Key
+                all.accept_err(TK.XID)
+                val lib = (all.tk0 as Tk.Id)
+                val arg = this.expr()
+                all.accept_err(TK.CHAR, ':')
+                val tp = this.type()
+                Stmt.Input(tk, tp, dst, lib, arg)
+            }
+            all.check(TK.SPAWN) -> {
+                val s = this.stmt()
+                All_assert_tk(s.tk, s is Stmt.SSpawn) { "unexpected dynamic `spawn`" }
+                val ss = s as Stmt.SSpawn
+                Stmt.SSpawn(ss.tk_, dst, ss.call)
+            }
+            else -> {
+                val src = this.expr()
+                Stmt.Set(tk0, dst, src)
+            }
+        }
+    }
+
     open fun stmt (): Stmt {
         return when {
             all.accept(TK.VAR) -> {
@@ -237,28 +261,7 @@ open class Parser
                 val dst = this.attr().toExpr()
                 all.accept_err(TK.CHAR, '=')
                 val tk0 = all.tk0 as Tk.Chr
-                when {
-                    all.check(TK.INPUT) -> {
-                        all.accept(TK.INPUT)
-                        val tk = all.tk0 as Tk.Key
-                        all.accept_err(TK.XID)
-                        val lib = (all.tk0 as Tk.Id)
-                        val arg = this.expr()
-                        all.accept_err(TK.CHAR, ':')
-                        val tp = this.type()
-                        Stmt.Input(tk, tp, dst, lib, arg)
-                    }
-                    all.check(TK.SPAWN) -> {
-                        val s = this.stmt()
-                        All_assert_tk(s.tk, s is Stmt.SSpawn) { "unexpected dynamic `spawn`" }
-                        val ss = s as Stmt.SSpawn
-                        Stmt.SSpawn(ss.tk_, dst, ss.call)
-                    }
-                    else -> {
-                        val src = this.expr()
-                        Stmt.Set(tk0, dst, src)
-                    }
-                }
+                this.set_tail(tk0, dst)
             }
             all.accept(TK.INPUT) -> {
                 val tk = all.tk0 as Tk.Key
